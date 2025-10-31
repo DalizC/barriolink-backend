fbb559f687ec40870b0a5a9c1eb16116120b8c57
David Zurita <d.zurita.c2@gmail.com>
2025-10-31 03:18:35 -0300

    refactor: Se renombra la app Space a Facility paraa mayor claridad de lo que representa.
    - Se actualizan las referencias de space a facility.
    - Se genera nueva migración con el renombre.

--- core/admin.py
+++ core/admin.py
@@ -40,3 +40,4 @@ class UserAdmin(BaseUserAdmin):
 admin.site.register(models.User, UserAdmin)
 admin.site.register(models.Event)
 admin.site.register(models.News)
+admin.site.register(models.Facility)
